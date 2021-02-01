dbeba9a1d0bafe67396f36a1345973761a9ecde7
codigosincero <codeasgamer@gmail.com>
2021-01-31 21:48:35 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-du9epe
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-du9epe)
---
+++ index.js
@@ -0,0 +1,8 @@
+// Import stylesheets
+import "./style.css";
+import "./codigo.js";
+// Write Javascript code!
+const appDiv = document.getElementById("app");
+appDiv.innerHTML = `
+    <script src="codigo.js"></script>
+<h1>JS Stsasrter</h1>`;
